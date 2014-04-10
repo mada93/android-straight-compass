f2f46ef17badd14c9fd62b01e355657a3cf8d847
hieronymus.sixtus@googlemail.com <hieronymus.sixtus@googlemail.com@efe48e52-6ace-c13b-553f-dbf436200e7c>
2014-04-10 13:55:19 +0000

    improved compass animation (zero crossing); added cardinal directions to heading degrees

--- trunk/StraightCompass/src/de/streblow/straightcompass/MainActivity.java
+++ trunk/StraightCompass/src/de/streblow/straightcompass/MainActivity.java
@@ -7,6 +7,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -92,12 +93,34 @@ public class MainActivity extends Activity implements SensorEventListener {
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 
-		// get the angle around the z-axis rotated
+		// get the angle around the z-axis rotated (relative to 0 degrees)
 		float degree = Math.round(event.values[0]);
 
-		tvHeading.setText("Heading: " + Float.toString(degree) + " degrees");
+		// get the sectors around the 8 main directions
+		int currentSector = (int) (-currentDegree / (360f / 16f));
+		int sector = (int) (degree / (360f / 16f));
+		String direction = "";
+		if (sector == 15 || sector == 0) direction = "N";
+		else if (sector == 1 || sector == 2) direction = "NE";
+		else if (sector == 3 || sector == 4) direction = "E";
+		else if (sector == 5 || sector == 6) direction = "SE";
+		else if (sector == 7 || sector == 8) direction = "S";
+		else if (sector == 9 || sector == 10) direction = "SW";
+		else if (sector == 11 || sector == 12) direction = "W";
+		else if (sector == 13 || sector == 14) direction = "NW";
+
+		tvHeading.setText(((int)degree) + "° " + direction);
 
 		// create a rotation animation (reverse turn degree degrees)
+
+		if (direction == "N")
+			// check possible zero-crossing
+			if (currentSector == 15 && sector == 0)
+				// crossing with the needle from W to E rotate image counter clockwise
+				degree = 360f + degree;
+			else if (currentSector == 0 && sector == 15)
+				// crossing with the needle from E to W rotate image clockwise
+				currentDegree = -360f - currentDegree;
 		RotateAnimation ra = new RotateAnimation(
 				currentDegree, 
 				-degree,
@@ -111,9 +134,12 @@ public class MainActivity extends Activity implements SensorEventListener {
 		// set the animation after the end of the reservation status
 		ra.setFillAfter(true);
 
+		// set the animation's interpolator to linear
+		ra.setInterpolator(new LinearInterpolator());
+
 		// Start the animation
 		image.startAnimation(ra);
-		currentDegree = -degree;
+		currentDegree = - (degree % 360f);
 
 	}
 
